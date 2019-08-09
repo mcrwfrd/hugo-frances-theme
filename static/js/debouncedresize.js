9e16eef401db0ee6013956b92c70eae95246a313
Michael Crawford <michael@crawford.io>
2019-08-09 11:27:30 -0700

    move debounceresized to own file

---
+++ static/js/debouncedresize.js
@@ -0,0 +1,40 @@
+/*
+* debouncedresize: special jQuery event that happens once after a window resize
+*
+* latest version and complete README available on Github:
+* https://github.com/louisremi/jquery-smartresize/blob/master/jquery.debouncedresize.js
+*
+* Copyright 2011 @louis_remi
+* Licensed under the MIT license.
+*/
+var $event = $.event,
+    $special,
+    resizeTimeout;
+
+$special = $event.special.debouncedresize = {
+    setup: function() {
+        $( this ).on( "resize", $special.handler );
+    },
+    teardown: function() {
+        $( this ).off( "resize", $special.handler );
+    },
+    handler: function( event, execAsap ) {
+        // Save the context
+        var context = this,
+            args = arguments,
+            dispatch = function() {
+                // set correct event type
+                event.type = "debouncedresize";
+                $event.dispatch.apply( context, args );
+            };
+
+        if ( resizeTimeout ) {
+            clearTimeout( resizeTimeout );
+        }
+
+        execAsap ?
+            dispatch() :
+            resizeTimeout = setTimeout( dispatch, $special.threshold );
+    },
+    threshold: 250
+};
